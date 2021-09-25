752e298be56c2f9bd55ce45c9aa449594b5d7de7
MrSemyon12 <denisenko.box19@gmail.com>
2021-09-25 20:47:39 +1000

    added Ball class ond mouse click event

--- CircleGame.py
+++ CircleGame.py
@@ -1,14 +1,32 @@
 import pygame
+from random import randint
 
 pygame.init()
-
 screen = pygame.display.set_mode((1600, 900))
 pygame.display.set_caption("CircleDrop")
+
+
+class Ball:
+    def __init__(self, pos):
+        self.pos = pos
+        self.radius = randint(10, 100)
+        self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        self.speed = 0
+
+    def draw_ball(self):
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
+
+    def fall_ball(self):
+        pass
+
 
 while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            ball = Ball(pygame.mouse.get_pos())
+            ball.draw_ball()
 
     pygame.display.update()
