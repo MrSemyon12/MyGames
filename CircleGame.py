e24a04edd7dd700412b8b461c7ca49b017cd5854
MrSemyon12 <denisenko.box19@gmail.com>
2021-09-25 22:44:53 +1000

    added balls fall

--- CircleGame.py
+++ CircleGame.py
@@ -1,32 +1,53 @@
 import pygame
 from random import randint
 
+window_size = (1600, 900)
 pygame.init()
-screen = pygame.display.set_mode((1600, 900))
+screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("CircleDrop")
+
+ball_list = list()
 
 
 class Ball:
     def __init__(self, pos):
-        self.pos = pos
+        self.pos_x = pos[0]
+        self.pos_y = pos[1]
         self.radius = randint(10, 100)
         self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
         self.speed = 0
 
     def draw_ball(self):
-        pygame.draw.circle(screen, self.color, self.pos, self.radius)
+        pygame.draw.circle(screen, self.color, (self.pos_x, self.pos_y), self.radius)
 
     def fall_ball(self):
-        pass
+        if self.pos_y < window_size[1]:
+            self.pos_y += 1
+        else:
+            self.pos_y = window_size[1]
+
+
+def draw_balls():
+    for element in ball_list:
+        element.draw_ball()
+
+
+def fall_balls():
+    for element in ball_list:
+        element.fall_ball()
 
 
 while True:
+    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             ball = Ball(pygame.mouse.get_pos())
-            ball.draw_ball()
+            ball_list.append(ball)
+
+    draw_balls()
+    fall_balls()
 
     pygame.display.update()
